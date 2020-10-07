30ff7771efced3c8d08ccf339e8c5d0a3f8486d4
Lisa Chang <jofan.chang@gmail.com>
2020-10-07 19:50:19 +0800

    Set retrievable local storage for today

--- js/app.js
+++ js/app.js
@@ -1,16 +1,45 @@
+const list = document.getElementById('glist');
+let idDate;
+
 function setDate(){
   let d = new Date();
   let month = d.getMonth() + 1;
+  idDate = month.toString() + d.getDate().toString() + d.getFullYear().toString();
   let newDate = month +'.'+ d.getDate()+'.'+ d.getFullYear();
   document.getElementById('date').innerHTML = newDate;
-  console.log(newDate);
-}
+  checkGoals();
+};
+
+function checkGoals(){
+  if(localStorage.getItem(idDate)){
+    obj = JSON.parse(localStorage.getItem(idDate));
+    // i = obj.length;
+    for (const goal of Object.values(obj)) {
+      const liSet = document.createElement('li');
+      liSet.textContent = goal;
+      list.appendChild(liSet);
+    };
+  };
+};
+
+let obj = {};
+let newGoal;
+const field = document.getElementById('new');
+// let i = 0;
 
 document.getElementById('form').addEventListener('submit', performAction);
 
 function performAction(event) {
-  console.log(event);
-  document.getElementById('one').textContent = `${event.timeStamp}`;
+  newGoal = field.value;
+  const key = Object.keys(obj).length + 1;
+  obj[key] = newGoal;
+  localStorage.setItem(idDate,JSON.stringify(obj));
+  const goalObj = JSON.parse(localStorage.getItem(idDate));
+  const liCurrent = document.createElement('li');
+  liCurrent.textContent = goalObj[key];
+  list.appendChild(liCurrent);
+  // i += 1;
+  field.value = '';
   event.preventDefault();
 };
 
